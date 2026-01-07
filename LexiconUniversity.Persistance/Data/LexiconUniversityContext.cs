ca3525124823b0b987e6d08ebf28892055156eae
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-07 13:45:09 +0100

    Manually added DbContext and migration to create db

---
+++ LexiconUniversity.Persistance/Data/LexiconUniversityContext.cs
@@ -0,0 +1,17 @@
+﻿using LexiconUniversity.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconUniversity.Persistance.Data
+{
+    public class LexiconUniversityContext : DbContext
+    {
+        public LexiconUniversityContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = default!; 
+    }
+}
