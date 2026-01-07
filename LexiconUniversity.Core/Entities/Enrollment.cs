190a4b6ade42287110b357e1b3d91786c5a62437
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-07 13:22:26 +0100

    Add project files.

---
+++ LexiconUniversity.Core/Entities/Enrollment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconUniversity.Core.Entities
+{
+    public class Enrollment
+    {
+        public int Grade { get; set; }
+    }
+}
