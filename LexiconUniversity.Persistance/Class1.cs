8129f25d27985e70d21925ec4dde275658e7fa6e
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-08 11:55:08 +0100

    Added SeedData

--- LexiconUniversity.Persistance/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LexiconUniversity.Persistance
-{
-    public class Class1
-    {
-
-    }
-}
--- LexiconUniversity.Web/Program.cs
+++ LexiconUniversity.Web/Program.cs
@@ -1,11 +1,13 @@
+using LexiconUniversity.Persistance;
 using LexiconUniversity.Persistance.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace LexiconUniversity.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,25 @@ namespace LexiconUniversity.Web
             options.UseSqlServer(builder.Configuration.GetConnectionString("LexiconUniversityContext") ?? throw new InvalidOperationException("Connection string 'LexiconUniversityContext' not found.")));
 
             var app = builder.Build();
+
+            using(var scope = app.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var context = serviceProvider.GetRequiredService<LexiconUniversityContext>();
+
+                //await context.Database.EnsureDeletedAsync();
+                //await context.Database.MigrateAsync();
+
+                try
+                {
+                    await SeedData.InitAsync(context); 
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
