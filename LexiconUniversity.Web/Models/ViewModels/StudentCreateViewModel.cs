497827bdc641560f0ab7608fddc3d9df5b7a8c60
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-09 10:49:44 +0100

    Added custom validation for streetnr

--- LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
+++ LexiconUniversity.Web/Models/ViewModels/StudentCreateViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace LexiconUniversity.Web.Models.ViewModels
+﻿using LexiconUniversity.Web.Validations;
+
+namespace LexiconUniversity.Web.Models.ViewModels
 {
     public class StudentCreateViewModel
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        [CheckStreetNr(10)]            
         public string Street { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
---
+++ LexiconUniversity.Web/Validations/CheckStreetNr.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LexiconUniversity.Web.Validations
+{
+    public class CheckStreetNr : ValidationAttribute
+    {
+        private readonly int _max;
+
+        public CheckStreetNr(int max)
+        {
+            _max = max;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if(value is string input)
+            {
+                var num = input.Trim().Split().Last();
+                return int.TryParse(num, out int res) & res <= _max; 
+            }
+            return false; 
+        }
+
+    }
+}
