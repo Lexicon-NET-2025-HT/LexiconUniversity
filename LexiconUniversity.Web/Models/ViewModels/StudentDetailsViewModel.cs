e1a7f750333349d873128fd2f51cb6956529d6de
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-09 10:26:09 +0100

    Created DetailsViewModel and added details back

---
+++ LexiconUniversity.Web/Models/ViewModels/StudentDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace LexiconUniversity.Web.Models.ViewModels
+{
+    public class StudentDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+
+        public int Attending { get; set; }
+
+        public List<Course> Courses { get; set; } = new List<Course>(); 
+
+    }
+}
