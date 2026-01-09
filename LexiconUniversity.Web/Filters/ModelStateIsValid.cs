6f6c03acaa90ceab121e66cd74b656b032ca5e09
Jonathan Krall <jonathan.krall@lexicon.se>
2026-01-09 11:27:21 +0100

    ModelStateIsValid-filter

---
+++ LexiconUniversity.Web/Filters/ModelStateIsValid.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LexiconUniversity.Web.Filters
+{
+    public class ModelStateIsValid : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new ViewResult
+                {
+                    StatusCode = 400,
+                    TempData = ((Controller)context.Controller).TempData,
+                    ViewData = ((Controller)context.Controller).ViewData
+                };
+            }
+        }
+    }
+}
